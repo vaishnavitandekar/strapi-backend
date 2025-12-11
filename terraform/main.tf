0f10380b05c877ffa06fa7205977a69061b43978
Vaishnavi Tandekar <vaishnavitandekar.devops@gmail.com>
2025-12-11 15:19:03 +0530

    Rename security group and update tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
+  name        = "strapi-sg-backend"
   description = "Allow Strapi and SSH"
 
   ingress {
@@ -40,7 +40,7 @@ resource "aws_instance" "strapi" {
   ]
 
   tags = {
-    Name = "Strapi-Server"
+    Name = "Vaishnavi-Strapi-Server"
   }
 }
 
